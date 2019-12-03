57f8af03f9cab6abd970fe5890a42841189564f6
Luka Dimnik <luka.dimnik88@gmail.com>
2019-12-03 17:02:19 +0200

    minor changes

--- src/main/java/com/example/ExercisePlanner/ExercisePlannerApplication.java
+++ src/main/java/com/example/ExercisePlanner/ExercisePlannerApplication.java
@@ -21,12 +21,12 @@ public class ExercisePlannerApplication {
 	public CommandLineRunner exerciseDemo(ExerciseRepository exRepository, WorkoutRepository workRepository) {
 		return (args) -> {
 
-			workRepository.save(new Workout("Workout 1"));
-			workRepository.save(new Workout("Workout 2"));
+			workRepository.save(new Workout("Fat Burn"));
+			workRepository.save(new Workout("Strength Training"));
 
-			exRepository.save(new Exercise("Bench Press", 22, 19, 30, workRepository.findByName("Workout 1")));
-			exRepository.save(new Exercise("Squats", 22, 19, 30, workRepository.findByName("Workout 1")));
-			exRepository.save(new Exercise("Bench Press", 22, 19, 30, workRepository.findByName("Workout 2")));
+			exRepository.save(new Exercise("Push Ups", 22, 19, 30, workRepository.findByName("Fat Burn")));
+			exRepository.save(new Exercise("Squats", 22, 19, 30, workRepository.findByName("Strength Training")));
+			exRepository.save(new Exercise("Bench Press", 22, 19, 30, workRepository.findByName("Strength Training")));
 
 		};
 	}
